46766a6ef5d6ed7ce23542690d1ab1cdf0f251c2
MateuszMazurTUL <MateuszMazurTUL@edu.p.lodz.pl>
2021-10-17 10:11:01 +0200

    basic draw and basic input

--- lab2.js
+++ lab2.js
@@ -1,143 +1,72 @@
-//canvas
-var ctx = $("#canvas")[0].getContext("2d");
-ctx.canvas.width  = 800;
-ctx.canvas.height = 600;
-
-
-//player
-class Player {
-    constructor (posX, posY, sizeX, sizeY) {
-        this.posX = posX;
-        this.posY = posY;
-        this.sizeX = sizeX;
-        this.sizeY = sizeY;
+$( window ).on( "load", function() {
+    //canvas
+    var ctx2 = $("#canvas")[0].getContext("2d");
+    ctx2.canvas.width  = 800;
+    ctx2.canvas.height = 600;
+    
+    var Config = {
+        moveLeft = 1,
+        moveRight = 2,
+        shoot = 3
     }
-}
+    
+    //event listener
+    document.addEventListener('keydown', function(event) {
+        if(e.keyCode==Config.moveLeft){
+        // move left
+        }
+        if(e.keyCode==Config.moveRight){
+        // move right
+        }
+        if(e.keyCode==Config.shoot){
+        // Enter pressed... do anything here...
+        }
+        
+    }, false);
 
-//enemy
-class Enemy {
-    constructor (posX, posY, radius) {
-        this.posX = posX;
-        this.posY = posY;
-        this.radius = radius;
+    //player
+    class Player {
+        constructor (posX, posY, sizeX, sizeY) {
+            this.posX = posX;
+            this.posY = posY;
+            this.sizeX = sizeX;
+            this.sizeY = sizeY;
+        }
+
+        draw() {
+            ctx2.beginPath();
+            ctx2.fillStyle = "black";
+            ctx2.rect(this.posX, this.posY, this.sizeX,this.sizeY); 
+            ctx2.fill();
+            ctx2.closePath();
+        }
     }
-}
 
-class Bullet {
-    constructor (posX, posY, radius) {
-        this.posX = posX;
-        this.posY = posY;
-        this.radius = radius;
+    //enemy
+    class Enemy {
+        constructor (posX, posY, radius) {
+            this.posX = posX;
+            this.posY = posY;
+            this.radius = radius;
+        }
     }
+
+    class Bullet {
+        constructor (posX, posY, radius) {
+            this.posX = posX;
+            this.posY = posY;
+            this.radius = radius;
+        }
+    }
+
+    function draw(){
+        ctx2.clearRect(0, 0, 800, 600);
+    }
+
+    const player = new Player (390, 550, 20, 40);
+
+
+
+    setInterval(draw(), 10);
+    setInterval(player.draw(), 10);
 }
-
-
-
-
-
-
-
-
-
-
-
-//bgn color
-ctx.backgroundColor = 'red';
-
-//color
-var r = 0,
-    r1 = 0,
-    g = 255,
-    g1 = 255,
-    color = "rgb("+r+","+g+",0)",
-    color1 = "rgb("+r+","+g+",0)";
-
-//flags for colors
-var isRedDown = false;
-var isGreenDown = true;
-
-//movement
-var speed = 3,
-    rad = 20,
-    moveY = speed;
-
-//clock
-var intervaltime = 10,
-    clock = 255/1000 * intervaltime;
-    
-
-//todo array
-var ball1 = { x: 200, y: 400 };
-var ball2 = { x: 400, y: 400 };
-var ball3 = { x: 600, y: 400 };
-
-function drawMe() {
-    //clear area
-    ctx.clearRect(0, 0, 800, 600);
-    
-    //change dir move
-    if (ball3.y < 200 || ball3.y > 600 - rad) moveY = -moveY
-
-    //ball.x += moveX;
-    ball3.y += moveY;
-    
-    //draw on canvas
-    ctx.beginPath();
-    ctx.fillStyle = color1;
-    ctx.arc(ball1.x, ball1.y, rad, 0, Math.PI * 2, false); 
-    ctx.fill();
-    ctx.closePath();
-    
-    ctx.fillStyle = color;
-    ctx.arc(ball2.x, ball2.y, rad, 0, Math.PI * 2, false);
-    ctx.fill();
-    ctx.closePath();
-    
-    ctx.fillStyle = color;
-    ctx.arc(ball3.x, ball3.y, rad, 0, Math.PI * 2, false);
-    ctx.fill();
-    ctx.closePath();
-    
-    
-    ctx.beginPath();
-    ctx.fillStyle = color1;
-    ctx.arc(ball1.x, ball1.y, rad, 0, Math.PI * 2, false); 
-    ctx.fill();
-    ctx.closePath();
-}
-
-function changeColor() {
-    //change gradient dir
-    if (r <= 0 || r >= 255) isRedDown = !isRedDown;
-    if (g <= 0 || g >= 255) isGreenDown = !isGreenDown;
-    
-    //change color smooth (gradient)
-    if (isRedDown) r+=clock;
-    else r-=clock;
-    
-    if (isGreenDown) g+=clock;
-    else g-=clock;
-    
-    color = "rgb("+r+","+g+",0)";
-    console.log(color1);
-}
-
-function changeColor1() {
-    //change color
-    if (r1 == 255) r1 = 0;
-    else r1 = 255;
-    
-    if (g1 == 255) g1 = 0;
-    else g1 = 255;
-    
-    
-    
-    color1 = "rgb("+r1+","+g1+",0)";
-    //console.log(color);
-}
-
-setInterval(changeColor, 10);
-setInterval(changeColor1, 1000);
-setInterval(drawMe, 10);
-
-//todo fixupdate
